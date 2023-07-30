80ad997697343400f3b0feb5dd0f1c4f4874edf8
hoanglog99 <longnhph18507@fpt.edu.vn>
2023-07-30 19:56:24 +0700

    clean code

--- routes/web.php
+++ routes/web.php
@@ -34,30 +34,6 @@ Route::group(['namespace' => 'Auth','prefix' => 'account'], function(){
     Route::get('/{social}/callback', 'SocialAuthController@callback')->name('get.login.social_callback');
 });
 
-Route::group(['prefix' => 'api-admin','namespace' => 'Admin','middleware' => 'check_admin_login'], function() {
-    Route::group(['prefix' => 'product'], function(){
-        Route::get('','AdminProductController@index')->name('admin.product.index');
-        Route::get('create','AdminProductController@create')->name('admin.product.create');
-        Route::post('create','AdminProductController@store');
-
-        Route::get('hot/{id}','AdminProductController@hot')->name('admin.product.hot');
-        Route::get('active/{id}','AdminProductController@active')->name('admin.product.active');
-        Route::get('update/{id}','AdminProductController@edit')->name('admin.product.update');
-        Route::post('update/{id}','AdminProductController@update');
-
-        Route::get('delete/{id}','AdminProductController@delete')->name('admin.product.delete');
-        Route::get('delete-image/{id}','AdminProductController@deleteImage')->name('admin.product.delete_image');
-    });
-
-    Route::group(['prefix' => 'transaction'], function(){
-        Route::get('','AdminTransactionController@index')->name('admin.transaction.index');
-        Route::get('delete/{id}','AdminTransactionController@delete')->name('admin.transaction.delete');
-        Route::get('order-delete/{id}','AdminTransactionController@deleteOrderItem')->name('ajax_admin.transaction.order_item');
-        Route::get('view-transaction/{id}','AdminTransactionController@getTransactionDetail')->name('ajax.admin.transaction.detail');
-        Route::get('action/{action}/{id}','AdminTransactionController@getAction')->name('admin.action.transaction');
-    });
-});
-
 Route::group(['namespace' => 'Frontend'], function() {
     Route::get('/','HomeController@index')->name('get.home');
     Route::get('san-pham','ProductController@index')->name('get.product.list');
@@ -73,3 +49,5 @@ Route::group(['namespace' => 'Frontend'], function() {
          Route::get('update/{id}','ShoppingCartController@update')->name('ajax_get.shopping.update');
      });
 });
+
+include 'route_admin.php';
