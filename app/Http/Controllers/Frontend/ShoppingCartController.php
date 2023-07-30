3757363ab7fb47a16e6efe4f9e766a0c8f82dc36
dutcph15015 <dutcph15015@fpt.edu.vn>
2023-07-30 22:23:27 +0700

    update order

--- app/Http/Controllers/Frontend/ShoppingCartController.php
+++ app/Http/Controllers/Frontend/ShoppingCartController.php
@@ -74,4 +74,96 @@ class ShoppingCartController extends Controller
             'size' => $request->size
         ]);
     }
+
+    // thuc hien thanh toan
+    public function postPay(Request $request)
+    {
+        $data = $request->except("_token", 'payment');
+        if (!\Auth::user()->id) {
+            //4. Thông báo
+            \Session::flash('toastr', [
+                'type'    => 'error',
+                'message' => 'Đăng nhập để thực hiện tính năng này'
+            ]);
+
+            return redirect()->back();
+        }
+        $data['tst_user_id'] = \Auth::user()->id;$data['tst_user_id'] = \Auth::user()->id;
+        $data['tst_total_money'] = str_replace(',', '', \Cart::subtotal(0));
+        $data['created_at']      = Carbon::now();
+
+        if ($request->payment == 2) {
+            $totalMoney = str_replace(',', '', \Cart::subtotal(0));
+            session(['info_custormer' => $data]);
+            return view('frontend/pages/vnpay/index', compact('totalMoney'));
+        } else {
+            $data['tst_code']      = randString(15);
+            $transactionID           = Transaction::insertGetId($data);
+            if ($transactionID) {
+                $shopping = \Cart::content();
+                // Mail::to($request->tst_email)->send(new TransactionSuccess($shopping));
+
+                foreach ($shopping as $key => $item) {
+
+                    // Lưu chi tiết đơn hàng
+                    Order::insert([
+                        'od_transaction_id' => $transactionID,
+                        'od_product_id'     => $item->id,
+                        'od_sale'           => $item->options->sale,
+                        'od_qty'            => $item->qty,
+                        'od_price'          => $item->price,
+                        'od_size'          => $item->options->size,
+                        'od_color'          => $item->options->color,
+                        'od_gender'          => $item->options->gender,
+                    ]);
+
+                    //Tăng pay ( số lượt mua của sản phẩm dó)
+                    // \DB::table('products')
+                    //     ->where('id', $item->id)
+                    //     ->increment("pro_pay");
+                    $product = Product::find($item->id);
+                    $qty = $item->qty + $product->pro_pay;
+                    $product = $product->update(['pro_pay' => $qty]);
+                }
+            }
+
+            \Session::flash('toastr', [
+                'type'    => 'success',
+                'message' => 'Đơn hàng của bạn đã được lưu'
+            ]);
+            \Cart::destroy();
+            return redirect()->to('/');
+        }
+    }
+    // up date don hang
+    public function update(Request $request, $id)
+    {
+        if ($request->ajax()) {
+
+            //1.Lấy tham số
+            $qty       = $request->qty ?? 1;
+            $idProduct = $request->idProduct;
+            $product   = Product::find($idProduct);
+
+            //2. Kiểm tra tồn tại sản phẩm
+            if (!$product) return response(['messages' => 'Không tồn tại sản sản phẩm cần update']);
+
+            //3. Kiểm tra số lượng sản phẩm còn ko
+            if ($product->pro_number < $qty) {
+                return response([
+                    'messages' => 'Số lượng cập nhật không đủ',
+                    'error'    => true
+                ]);
+            }
+
+            //4. Update
+            \Cart::update($id, $qty);
+
+            return response([
+                'messages'   => 'Cập nhật thành công',
+                'totalMoney' => \Cart::subtotal(0),
+                'totalItem'  => number_format(number_price($product->pro_price, $product->pro_sale) * $qty, 0, ',', '.')
+            ]);
+        }
+    }
 }
