a72b88434bb9f28c85becea51e84483cf19f82d8
conmeowngungoc <101876056+conmeowngungoc@users.noreply.github.com>
2023-08-20 00:26:26 +0700

    fix name

--- app/Http/Controllers/Admin/AdminRatingController.php
+++ app/Http/Controllers/Admin/AdminRatingController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AdminRatringController extends Controller
+class AdminRatingController extends Controller
 {
     public function index()
     {
