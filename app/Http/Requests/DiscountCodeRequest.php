779014066ab5d105254c0d1afee0ad815d72be26
hoanglog99 <longnhph18507@fpt.edu.vn>
2023-09-09 04:23:08 +0700

    fix validate discount code

--- app/Http/Requests/DiscountCodeRequest.php
+++ app/Http/Requests/DiscountCodeRequest.php
@@ -27,8 +27,8 @@ class DiscountCodeRequest extends FormRequest
         $validate = [
             //
             'd_code'         => 'required|max:190|min:1|unique:discount_code,d_code,'.$this->id,
-            'd_percentage'  => 'required',
-            'd_number_code'  => 'required'
+            'd_percentage'  => 'required|max:99|min:1',
+            'd_number_code'  => 'required|min:1'
         ];
 
         return $validate;
@@ -43,7 +43,10 @@ class DiscountCodeRequest extends FormRequest
             'd_code.max'            => 'Vượt quá số ký tự cho phép',
             'd_code.unique' => 'Mã code đã bị trùng',
             'start_date.after'            => 'Ngày bắt đầu phải lớn hơn ngày hiện tại',
-            'end_date.after'            => 'Ngày kết thúc phải lớn hơn ngày bắt đầu và ngày hiện tại'
+            'end_date.after'            => 'Ngày kết thúc phải lớn hơn ngày bắt đầu và ngày hiện tại',
+            'd_percentage.max' => 'Giá trị phải thuộc từ 1 đến 99',
+            'd_percentage.min' => 'Giá trị phải thuộc từ 1 đến 99',
+            'd_number_code.min' => 'số lượng phải lớn hơn 1'
         ];
     }
 }
